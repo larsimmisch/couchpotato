52c1739f40f6df8526736f6287fe1876542587f1
Lars Immisch <lars@ibp.de>
2010-03-22 17:05:10 +0100

    Simple by-title index.

--- views/by-title/map.js
+++ views/by-title/map.js
@@ -6,15 +6,7 @@ function(doc) {
 		}
 	}
 
-	if (doc.name) {
-		if (doc.name.given) {
-			emit_split(doc.name.given, doc)
-		}
-		if (doc.name.additional) {
-			emit_split(doc.name.additional, doc)
-		}
-		if (doc.name.family) {
-			emit_split(doc.name.family, doc);
-		}
+	if (doc.title) {
+		emit_split(doc.title, doc)
 	}
 }
