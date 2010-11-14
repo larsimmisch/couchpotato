6cc1452de4c7a23a65270af0dc28333f146f2a4d
Lars Immisch <lars@ibp.de>
2010-11-15 00:23:05 +0100

    Client-side path fixed.

--- _attachments/path.js
+++ _attachments/path.js
@@ -1,12 +1,63 @@
-//path functions for use in browser-side javascript
-function assetPath() {
-	return document.location.pathname.split('/').slice(0, 3)
+// path functions for use in browser-side javascript
+
+// adapted from couch.js
+function encodeOptions(options) {
+	var buf = []
+	if (typeof(options) == "object" && options !== null) {
+		for (var name in options) {
+			if (!options.hasOwnProperty(name)) continue;
+			var value = options[name];
+			if (name == "key" || name == "startkey" || name == "endkey") {
+				value = toJSON(value);
+			}
+			buf.push(encodeURIComponent(name) + "=" + encodeURIComponent(value))
+		}
+	}
+	if (!buf.length) {
+		return ""
+	}
+	return "?" + buf.join("&");
 }
 
+function concatArgs(array, args) {
+	for (var i=0; i < args.length; i++) {
+		array.push(args[i]);
+	}
+	return array
+}
+
+function makePath(array) {
+	var options, path
+  
+	if (typeof array[array.length - 1] != "string") {
+		// it's a params hash
+		options = array.pop()
+	}
+	path = array.map(function(item) {
+		return encodeURIComponent(item) }).join('/')
+	if (options) {
+		return document.location.protocol + '//' + document.location.host
+			+ path + encodeOptions(options)
+	} else {
+		return document.location.protocol + '//' + document.location.host
+			+ path
+	}
+}
+
+function assetPath() {
+	var p = document.location.pathname.split('/')
+	var parts = [p[0], p[1] , p[2], p[3]];
+	return makePath(concatArgs(parts, arguments));
+};
+
 function showPath() {
-	return assetPath().push('_show')
+	var p = document.location.pathname.split('/')
+	var parts = [p[0], p[1] , p[2], p[3], '_show']
+	return makePath(concatArgs(parts, arguments))
 }
 
 function listPath() {
-	return assetPath().push('_list')
+	var p = document.location.pathname.split('/')
+	var parts = [p[0], p[1] , p[2], p[3], '_list']
+	return makePath(concatArgs(parts, arguments))
 }
